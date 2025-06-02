fc84260fa992356f22cb63146e0bec608cf716c5
nobodycp <hazmaniem@gmail.com>
2025-06-02 03:29:21 +0300

    🔁 Sync local changes to GitHub (2025-06-02 03:29:19)

--- tracker/templatetags/slugify_filters.py
+++ tracker/templatetags/slugify_filters.py
@@ -1,17 +1,9 @@
 from django import template
-from django.utils.text import slugify as django_slugify
-
-register = template.Library()
-
-@register.filter
-def slugify_filter(value):
-    return django_slugify(value)
-
-from django import template
 from django.utils.text import slugify
 
 register = template.Library()
 
+# تعيين أسماء أيقونات مخصصة
 ICON_OVERRIDES = {
     "mac": "macos",
     "windows": "windows",
@@ -23,12 +15,22 @@ ICON_OVERRIDES = {
     "firefox": "firefox",
     "safari": "safari",
     "opera": "opera",
-    "edge": "edge"
+    "edge": "edge",
 }
 
 @register.filter
 def icon_name(value):
+    """
+    يستخرج اسم الأيقونة المناسب من أول كلمة في قيمة المتصفح أو النظام.
+    """
     if not value:
         return "unknown"
-    name = value.lower().split(" ")[0]  # يلتقط أول كلمة فقط مثل "Chrome"
-    return ICON_OVERRIDES.get(name, slugify(name))
+    name = value.lower().split(" ")[0]  # التقاط أول كلمة فقط
+    return ICON_OVERRIDES.get(name, slugify(name))  # استخدام override أو slugify تلقائي
+
+@register.filter
+def slugify_filter(value):
+    """
+    يحول النص إلى slug صالح للاستخدام كـ class أو اسم ملف.
+    """
+    return slugify(value)
